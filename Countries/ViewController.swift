ff43cca5c23d51635591e6d1875a2900e7774ba7
andressancho <asancho@soin.co.cr>
2022-12-15 21:29:54 -0600

    Initial Commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Andres Sancho on 15/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
